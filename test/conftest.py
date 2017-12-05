610afbb4179e69d5e11fe1121f963156402df39d
Marek Jarycki <marek.jarycki@nsn.com>
2017-12-05 11:19:35 +0100

    Revert "tmp tests"
    This reverts commit 50c28bf7ef9cd2c7130bc374e8d39d3efb5b8abe.

--- test/conftest.py
+++ test/conftest.py
@@ -1,9 +1,5 @@
 import pytest
 import subprocess
-from contextlib import contextmanager
-from utils import writefile
-from os.path import join
-from dir_compare import wait_dirs_equal
 
 
 @pytest.fixture
@@ -16,26 +12,10 @@ def remote_dir(tmpdir_factory):
     return tmpdir_factory.mktemp('remote').strpath
 
 
-@contextmanager
-def dynsync_process(local_dir, remote_dir, opts=[]):
-    try:
-        p = subprocess.Popen(["dynsync", local_dir, "localhost:"+remote_dir] + opts)
-        yield p
-    finally:
-        if p.poll():
-            raise Exception("process died before exit")
-        p.terminate()
-
-
-@contextmanager
-def dynsync_process_initialized(local_dir, remote_dir, opts=[]):
-    writefile(join(local_dir, "anyfile_746329435435"))
-    with dynsync_process(local_dir, remote_dir, opts) as ds:
-        wait_dirs_equal(local_dir, remote_dir)
-        yield ds
-
-
 @pytest.yield_fixture
 def dynsync(local_dir, remote_dir):
-    with dynsync_process_initialized(local_dir, remote_dir) as ds:
-        yield ds
+    try:
+        p = subprocess.Popen(["dynsync", local_dir, "localhost:"+remote_dir])
+        yield p
+    finally:
+        p.terminate()
